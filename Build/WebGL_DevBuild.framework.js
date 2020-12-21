5213c17712ddfcd053c8caae23231bd8ac6fd40a
Jens Eeckhout <jenseeckhout2009@hotmail.com>
2020-12-21 17:47:40 +0100

    Update WebGL_DevBuild.framework.js

--- Build/WebGL_DevBuild.framework.js
+++ Build/WebGL_DevBuild.framework.js
@@ -3607,7 +3607,7 @@ function _JS_WebCamVideo_Start(deviceId) {
  }
     var video = document.createElement("video");
     navigator.getMedia({
-        video: { facingMode: {"environment" } },
+        video: true,
         audio: false
     }, (function(stream) {
         video.srcObject = stream;
